775c884fb9a7a94c3add2e918eab75ca2b88ef0e
AlexPa00 <nuve98@gmail.com>
2022-11-08 01:31:16 -0300

    cambios en gameMemory

--- src/JuegoDeMemoria/GameMemory.js
+++ src/JuegoDeMemoria/GameMemory.js
@@ -8,7 +8,10 @@ import ConstruirBaraja from './utils/ConstruirBaraja';
 const getEstadoInicial = ()=> {
   const baraja = ConstruirBaraja();
   return{
-    baraja
+    baraja,
+    //Propiedad de las parejas de cartas que se estan comparando
+    parejaSeleccionada: [], //Este es un array que puede contener un maximo de 2 elementos, ya que podemos comparar dos cartas a la vez.
+    estaComparando: false //Declaramos un boolean para que una vez que el usuario elija 2 cartas esta se vuelva "verdadero" mientras que la aplicacion calcula si ambas son iguales.(ya que no se quiere que el usuario pueda seguir eligiendo mientras se esta comparando)
   };
 }
 
@@ -25,11 +28,30 @@ render() {
       <Header/>
       <Tablero
       baraja={this.state.baraja}
+      parejaSeleccionada = {this.state.parejaSeleccionada} //esta siendo comparada, entonces se voltea para ver la carta seleccionada
+      seleccionarCarta = {(carta) => this.seleccionarCarta(carta)} // sirve para hacer referencia de la carta seleccionada al voltearla
       />
     </div>
   );
 }
+
+seleccionarCarta(carta)
+{
+  //Condiciones en las que el usuario no podria seleccionar ...
+  //--Mientras la aplicacion esta comparando no debemos permitir que el usuario seleccione una carta.
+  //--Mientras la carta ya fue seleccionada , es decir, si ya se encuentra en el array de parejas seleccionadas.
+  //No queremos que el usuario escoja cartas que ya fueron adivinadas.
+
+  if(this.state.estaComparando || this.state.parejaSeleccionada.indexOf(carta) >-1 || carta.fueAdivinada)
+  {
+    return; //Si alguna de estas 3 condiciones fueran ciertas , retornara.
+  }
+  const parejaSeleccionada = [...this.state.parejaSeleccionada,carta]; //actualizamos el arrays de las parejas seleccionadas
+  this.setState({
+    parejaSeleccionada  //actualizamos el estado que serai la propiedad parejaseleccionada
+  })
+} 
 }
 
 
-export default GameMemory;
+export default GameMemory; 
