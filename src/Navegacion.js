1e18b63adc0857f0a416aec9b89a8d10efb3865e
AlexPa00 <nuve98@gmail.com>
2022-11-13 23:35:19 -0300

    cambioa finales

--- src/Navegacion.js
+++ src/Navegacion.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
-import '../Navegacion.css';
-import Inicio from "./App";
-import Ahorcadito from "../ahorcadito/src/componentes/Inicio";
-import AtrapaComida from "./proyecto-phaser-final/src/game"
-import Arkkanoid from "./ArkanoidGame/game";
+//import '../Navegacion.css';
+import App from "./App";
+import Ahorcadito from "./ahorcadito/src/App";
+import AtrapaComida from "./proyecto-phaser-final/src/App"
+import Arkkanoid from "./ArkanoidGame/App";
 import PiPaTi from "./PiedraPapelTijera_GRUPO4-main/src/components/game";
 import JuegoMemoria from "./JuegoDeMemoria/GameMemory";
 
@@ -17,22 +17,22 @@ class Navegacion extends React.Component{
             <h1 className="Titulo">MenuPrincipal</h1>
             <nav>  
             <Link to ='/' className = "Boton" >Inicio</Link>
-            <Link to ='/Ahorcadito' className = "Boton" >Jugar</Link>
-            <Link to ='/AtrapaComida' className = "Boton" >Jugar</Link>
-            <Link to ='/Arkkanoid' className = "Boton" >Jugar</Link>
-            <Link to ='/PiPaTi' className = "Boton" >Jugar</Link>
-            <Link to ='/JuegoMemoria' className = "Boton" >Jugar</Link>
- 
+            <Link to ='/Ahorcadito' className = "Boton" >ahorcadito</Link>
+            <Link to ='/AtrapaComida' className = "Boton" >AtrapaComida</Link>
+            <Link to ='/Arkkanoid' className = "Boton" >Arkkanoid</Link>
+            <Link to ='/PiPaTi' className = "Boton" >PiPaTi</Link>
+            <Link to ='/JuegoMemoria' className = "Boton" >JuegoMemoria</Link>
             </nav>
         </div>
     
-        <Routes>
-            <Route path = "/" element= {<Inicio/>}/>
-            <Route path = "Ahorcadito" element= {<Ahorcadito/>}/>
-            <Route path = "AtrapaComida" element= {<AtrapaComida/>}/>
-            <Route path = "Arkkanoid" element= {<Arkkanoid/>}/>
-            <Route path = "PiPaTi" element= {<PiPaTi/>}/>
-            <Route path = "JuegoMemoria" element= {<JuegoMemoria/>}/>
+        
+            <Routes>
+            <Route path = "/" element= {<App/>}/>
+            <Route path = "/Ahorcadito" element= {<Ahorcadito/>}/>
+            <Route path = "/AtrapaComida" element= {<AtrapaComida/>}/>
+            <Route path = "/Arkkanoid" element= {<Arkkanoid/>}/>
+            <Route path = "/PiPaTi" element= {<PiPaTi/>}/>
+            <Route path = "/JuegoMemoria" element= {<JuegoMemoria/>}/>
         </Routes>
      </BrowserRouter>
      </>
